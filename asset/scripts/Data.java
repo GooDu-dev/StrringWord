f91891f0f14d10d8074daa82555d4488ec5fcf9b
GooDu-dev <mokumokutv@gmail.com>
2023-03-21 10:27:38 +0700

    Add word.java and data.java
    word.java for loading word and get a random word
    data.java for loading save and get data from save file

---
+++ asset/scripts/Data.java
@@ -0,0 +1,60 @@
+package asset.scripts;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Data {
+    private static final String PATH = "./asset/data/data.txt";
+    private static Map<String, Integer> data = new HashMap<String, Integer>();
+    private static final String CHAPTER = "chapter", EPISODE = "episode", HIGHEST_SCORE = "highestScore";
+
+    private static Map<String, Integer> loadSaved(){
+        Map<String, Integer> d = new HashMap<String, Integer>();
+        try{
+            Scanner file = new Scanner(new File(PATH));
+            do{
+                String[] dt = file.nextLine().split(":");
+                d.put(dt[0], Integer.parseInt(dt[1]));
+            }
+            while(file.hasNextLine());
+            return d;
+        }
+        catch(FileNotFoundException | NoSuchElementException e){
+            System.out.println(e.getMessage());
+            try{
+                FileWriter fw = new FileWriter(new File(PATH));
+                fw.write("chapter:1\n");
+                fw.write("episode:1\n");
+                fw.write("highestScore:0\n");
+                fw.close();
+                return loadSaved();
+            }
+            catch(IOException err){
+                System.out.println(err.getMessage());
+            }
+        }
+        return null;
+    }
+    public static Map<String, Integer> getData(){
+        if(data.size() == 0) data = loadSaved();
+        return data;
+    }
+    public static int getChapter(){
+        if(!data.keySet().contains(CHAPTER)) return -1;
+        return data.get(CHAPTER);
+    }
+    public static int getEpisode(){
+        if(!data.keySet().contains(EPISODE)) return -1;
+        return data.get(EPISODE);
+    }
+    public static int getScore(){
+        if(!data.keySet().contains(HIGHEST_SCORE)) return -1;
+        return data.get(HIGHEST_SCORE);
+    }
+}
---
+++ asset/scripts/Word.java
@@ -0,0 +1,36 @@
+package asset.scripts;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Word {
+    private static final String PATH = "./asset/data/word.txt";
+    private static ArrayList<String> words = new ArrayList<String>();
+
+    private static ArrayList<String> loadWord(){
+        ArrayList<String> data = new ArrayList<String>();
+        try{
+            Scanner file = new Scanner(new File(PATH));
+            while(file.hasNextLine()){
+                data.add(file.nextLine());
+            }
+            return data;
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    public static ArrayList<String> getWords(){
+        if(words.size() == 0) words = loadWord();
+        return words;
+    }
+    public static String getRandomWord(){
+        if(words.size() == 0) words = loadWord();
+        int index = (new Random().nextInt(words.size()+1)*100000)%(words.size()+1);
+        return words.get(index);
+    }
+}
