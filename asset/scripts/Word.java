2f40212c5818604f3aa0f2af5534a66755fda627
GooDu-dev <mokumokutv@gmail.com>
2023-03-27 03:30:49 +0700

    fixed bot

--- asset/scripts/Word.java
+++ asset/scripts/Word.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 public class Word {
     private static final String PATH = "./asset/data/word.txt";
     private static ArrayList<String> words = new ArrayList<String>();
+    protected static ArrayList<String> usedWords = new ArrayList<String>();
+
+    public static String word="";
 
     private static ArrayList<String> loadWord(){
         ArrayList<String> data = new ArrayList<String>();
@@ -31,6 +34,8 @@ public class Word {
     public static String getRandomWord(){
         if(words.size() == 0) words = loadWord();
         int index = (new Random().nextInt(words.size()+1)*100000)%(words.size()+1);
+        if(usedWords.contains(words.get(index))) return getRandomWord();
+        usedWords.add(words.get(index));
         return words.get(index);
     }
 }
